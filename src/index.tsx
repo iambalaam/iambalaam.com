df9003150acf5ee0ccae24951ce8f46ff9e4efd7
Guy Balaam <guybal@amazon.com>
2019-08-14 20:32:14 +0100

    Publishing inside Github action

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { renderToStaticMarkup } from 'react-dom/server';
 const H1: React.ComponentType = ({ children }) => <h1>{children}</h1>
 
 const handleRequest = async (_request: FetchEvent) => {
-    const html: string = renderToStaticMarkup(<H1>Hello React!</H1>);
+    const html: string = renderToStaticMarkup(<H1>Hello Github actions!</H1>);
     return new Response(html, { status: 200, headers: { 'content-type': 'text/html; charset=UTF-8' } });
 }
 self.addEventListener('fetch', (event) => {
