0f0fee3f550024b7212aa74f432e92de7a362af9
Guy Balaam <guybal@amazon.com>
2019-10-22 22:09:05 +0100

    Adding content-type to static worker

--- src/workers/static.ts
+++ src/workers/static.ts
@@ -11,14 +11,46 @@ declare var self: CloudflareWorkerGlobalScope;
 const ASSET_ROOT = '/static/';
 const ASSET_HOSTING_URL = 'https://raw.githubusercontent.com/iambalaam/iambalaam.com';
 
+const contentType = (asset: string): string => {
+    const match = asset.match(/(\.\w+(\.map)?)$/);
+    const extension = match && match[0];
+
+    switch (extension) {
+        // src files
+        case '.js': return 'application/javascript';
+        case '.css': return 'text/css';
+        // images
+        case '.bmp': return 'image/bmp';
+        case '.gif': return 'image/gif';
+        case '.jpg': return 'image/jpeg';
+        case '.jpg': return 'image/jpeg';
+        case '.png': return 'image/png';
+        case '.svg': return 'image/svg+xml';
+        case '.webp': return 'image/webp';
+        // map files
+        case '.js.map':
+        case '.css.map':
+            return 'application/json';
+        default:
+            return '';
+    }
+}
+
 const handleRequest = async (event: FetchEvent) => {
     const { pathname } = new URL(event.request.url);
     if (pathname && pathname.startsWith(ASSET_ROOT)) {
         const assetPath = pathname.slice(ASSET_ROOT.length);
-        return fetch(`${ASSET_HOSTING_URL}/${assetPath}`);
-    } else {
-        return new Response('Resource not found', { status: 404 });
+        const response = await fetch(`${ASSET_HOSTING_URL}/${assetPath}`);
+        if (response.ok) {
+            const content = await response.body;
+            return new Response(content, {
+                headers: {
+                    'content-type': contentType(pathname)
+                }
+            })
+        }
     }
+    return new Response('Resource not found', { status: 404 });
 }
 
 self.addEventListener('fetch', event => {
