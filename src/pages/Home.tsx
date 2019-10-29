5d0df73d0c3bc91e0f2ee95186521f92d706d118
Guy Balaam <guybal@amazon.com>
2019-10-29 13:49:08 +0000

    Adding programming animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,28 @@
 import * as React from 'react';
-import * as styles from './Home.module.scss';
+import * as s from './Home.module.scss';
 
 export default () =>
     <>
-        <h1>iambalaam.com</h1>
-        <p>Latest build commit: <span className={styles.red}>{BUILD_COMMIT}</span></p>
+        <h1>in development...</h1>
+        <div id="screen">
+            <div id="code">
+                <div className={s.line}><span className={`${s.xs} ${s.purple}`}></span><span className={`${s.m} ${s.blue}`}></span></div>
+                <div className={s.line}><span className={s.tab}></span><span className={`${s.l} ${s.red}`}></span></div>
+                <div className={s.line}><span className={s.tab}></span><span className={s.tab}></span><span className={`${s.xl} ${s.yellow}`}></span></div>
+                <div className={s.line}><span className={s.tab}></span><span className={`${s.m} ${s.red}`}></span></div>
+                <div className={s.line}><span className={s.tab}></span><span className={`${s.m} ${s.red}`}></span></div>
+                <div className={s.line}></div>
+                <div className={s.line}></div>
+                <div className={s.line}><span className={`${s.xs} ${s.purple}`}></span><span className={`${s.s} ${s.blue}`}></span></div>
+                <div className={s.line}><span className={s.tab}></span><span className={`${s.s} ${s.red}`}></span><span className={`${s.s} ${s.yellow}`}></span><span className={`${s.xs} ${s.red}`}></span></div>
+                <div className={s.line}><span className={s.tab}></span><span className={s.tab}></span><span className={`${s.m} ${s.red}`}></span></div>
+                <div className={s.line}><span className={s.tab}></span><span className={s.tab}></span><span className={`${s.xl} ${s.red}`}></span></div>
+                <div className={s.line}></div>
+                <div className={s.line}></div>
+                <div className={s.line}><span className={`${s.xs} ${s.purple}`}></span><span className={`${s.m} ${s.blue}`}></span></div>
+                <div className={s.line}><span className={s.tab}></span><span className={`${s.l} ${s.red}`}></span></div>
+                <div className={s.line}><span className={s.tab}></span><span className={s.tab}></span><span className={`${s.xl} ${s.yellow}`}></span></div>
+                <div className={s.line}><span className={s.tab}></span><span className={`${s.m} ${s.red}`}></span></div>
+            </div>
+        </div>
     </>
