ea3ca59f371a06e93e8d221d31eb4829fc11adcc
Guy Balaam <guybal@amazon.com>
2019-10-30 16:45:04 +0000

    Adding github link

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { Switch, Route } from 'react-router-dom';
 import './App.module.scss';
 import Home from './pages/Home';
 import NotFound from './pages/404';
+import GithubCorner from './components/GithubCorner';
 
 const App = () =>
     <main>
+        <GithubCorner/>
         <Switch>
             <Route exact path='/' component={Home} />
             <Route render={(routeProps) => (<NotFound {...routeProps} />)} />
