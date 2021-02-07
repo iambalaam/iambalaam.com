827e9c83e35910dd733a769ceb03a93a9b0b448e
Guy Balaam <guy.balaam@gmail.com>
2021-02-07 20:26:37 +0000

    Adding Jousting gif

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import Project from '../components/Project';
-import { Pencil, RewiredBot, Tick } from '../components/Models';
+import { Pencil, RewiredBot, Tick, WebsiteModel } from '../components/Models';
 
 import RewiredLogo from '../../static/rewired-logo.jpg';
 import RewiredFallOff from '../../static/rewired-fall-off.gif';
+
+import Jousting from '../../static/jousting.gif';
 
 import Cat from '../../static/photoshop-js-cat.gif';
 import Lightsaber from '../../static/lightsaber.gif';
@@ -13,10 +15,16 @@ export default function Home() {
         <>
             <h1>Projects</h1>
             <Project
-                title="Rewired"
+                title="GMTK GameJam 2020: Rewired"
                 techUsed={['Unity', 'Itch.io']}
                 frame={<RewiredBot />}
                 images={[RewiredLogo, RewiredFallOff]}
+            />
+            <Project
+                title="GMTK GameJam 2019: Jousting"
+                techUsed={['P5.js', 'Itch.io']}
+                frame={<WebsiteModel />}
+                images={[Jousting]}
             />
             <Project
                 title="photoshop-js"
