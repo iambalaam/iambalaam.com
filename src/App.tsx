c4d960550965bb27d7cff9070f42c57e38e42130
Guy Balaam <guy.balaam@gmail.com>
2021-02-09 16:36:03 +0000

    Adding navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.module.scss';
 
 import Header from './components/Header';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Experience from './pages/Experience';
+import Contact from './pages/Contact';
 import NotFound from './pages/404';
 
 import GithubCorner from './components/GithubCorner';
@@ -16,9 +17,11 @@ const App = () =>
         <main>
             <GithubCorner />
             <Switch>
-                <Route exact path={['/', '/about']} component={About} />
+                <Redirect exact path="/" to="/about" />
+                <Route exact path='/about' component={About} />
                 <Route exact path='/projects' component={Projects} />
                 <Route exact path='/experience' component={Experience} />
+                <Route exact path='/contact' component={Contact} />
                 <Route render={(routeProps) => (<NotFound {...routeProps} />)} />
             </Switch>
         </main>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
+import { NavLink, useLocation } from 'react-router-dom'
 
 import * as s from './Header.module.scss';
 
 export default function Header() {
-    return (
-        <header className={s.header}>
-            Guy Balaam - Software Developer
-        </header>
-    );
+    const path: string = useLocation().pathname;
+    return (<>
+        <header className={s.header}> Guy Balaam - Software Developer </header>
+        <nav>{
+            ['About', 'Projects', 'Experience', 'Contact'].map((Route) => {
+                const route = Route.toLowerCase();
+                const target = '/' + route;
+                return (path === target)
+                    ? <span>{Route}</span>
+                    : <NavLink to={target}>{Route}</NavLink>
+            })
+        }</nav>
+    </>);
 }
