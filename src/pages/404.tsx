67e365cce1cea6f7e342ef52bce3482a36b0688b
Guy Balaam <guy.balaam@gmail.com>
2021-02-14 13:16:20 +0000

    Updating 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router'
+import Project from '../components/Project';
 
 const NotFound: React.SFC<RouteComponentProps> = (props: RouteComponentProps) => {
     const { staticContext, location } = props;
@@ -8,10 +9,13 @@ const NotFound: React.SFC<RouteComponentProps> = (props: RouteComponentProps) =>
         ? location.pathname
         : 'this page'
     return (
-        <main>
-            <h1>Page not found</h1>
-            <p>Apologies, I haven't made {page} yet!</p>
-        </main>
+        <Project
+            title="404"
+            techUsed={[]}
+            frame={<></>}
+            tagline="Page not found"
+            description={`Apologies, I haven't made ${page} yet!`}
+        />
     )
 }
 
