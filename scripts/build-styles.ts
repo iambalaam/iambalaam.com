c5625910c2d271225e89ca6752307b320a545a66
Guy Balaam <guy.balaam@gmail.com>
2025-03-01 20:37:52 +0000

    Adding sass build

--- scripts/build-styles.ts
+++ scripts/build-styles.ts
@@ -1,5 +1,22 @@
+import { parseArgs } from "jsr:@std/cli/parse-args";
+const args = parseArgs(Deno.args, { string: "outDir" });
+
+if (!("outDir" in args)) throw new Error("missing argument --outDir");
+try {
+  await Deno.readDirSync(args.outDir ?? "");
+} catch (e) {
+  throw new Error(`Can't read outDir '${args.outDir}'`);
+}
+
 import * as sass from "sass";
 import { walk } from "jsr:@std/fs/walk";
+import { createHash } from "node:crypto";
+
+function hashString(s: string) {
+  const hash = createHash("sha256");
+  hash.update(s);
+  return hash.digest("hex").slice(0, 16);
+}
 
 let styles = "";
 for await (const file of walk("src")) {
@@ -10,3 +27,9 @@ for await (const file of walk("src")) {
 }
 
 styles = styles.replace(/\s+/g, " ");
+const hash = hashString(styles);
+
+await Deno.writeFile(
+  `${args.outDir}/${hash}.css`,
+  new TextEncoder().encode(styles),
+);
